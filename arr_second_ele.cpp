d750ae219fa9e10c5cf4a3b4648d331bf9cce733
dujram17 <dujramsahu883@gmail.com>
2026-09-22 18:09:20 +0530

    program to find the second max element of an array.

---
+++ arr_second_ele.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+    int max = arr[0];
+    int secondmax = arr[0];
+    for(int i = 1;i<n;i++){
+        if(arr[i]>max){
+            secondmax = max;
+            max = arr[i];
+        }
+        else if(arr[i]>secondmax){
+            secondmax = arr[i];
+        }
+    }
+    cout<<"Secondmax of arr:"<<secondmax<<endl;
+
+    return 0;
+}
